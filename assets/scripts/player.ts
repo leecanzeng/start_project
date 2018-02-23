ad6e90a3ac1f38d43631ea7e5ef8f3ce2a7aaefe
leecanzeng <leecan_zeng@qq.com>
2018-02-23 19:31:08 +0800

    Initial commit

---
+++ assets/scripts/player.ts
@@ -0,0 +1,63 @@
+// 2018
+const { ccclass, property } = cc._decorator;
+
+@ccclass
+export default class Player extends cc.Component {
+
+    // 主角跳跃高度
+    @property
+    jumpHeight = 0    
+
+    // 主角跳跃持续时间
+    @property
+    jumpDuration = 0
+
+    // 辅助形变动作时间
+    @property
+    squashDuration = 0
+
+    // 最大移动速度
+    @property
+    maxMoveSpeed = 0
+
+    // 加速度
+    @property
+    accel = 0
+
+    // 跳跃音效资源
+    @property({
+        url: cc.AudioClip
+    })
+    jumpAudio = ''
+
+    // 加速度方向开关
+    accLeft = false;
+    accRight = false;
+
+    // 主角当前水平方向速度
+    xSpeed = 0;
+    // screen boundaries
+    minPosX = 0;
+    maxPosX = 0;
+
+    // 初始化跳跃动作
+    jumpAction = null;
+    
+    setJumpAction() {
+        // 跳跃上升
+        var jumpUp = cc.moveBy(this.jumpDuration, cc.p(0, this.jumpHeight)).easing(cc.easeCubicActionOut());
+        // 下落
+        var jumpDown = cc.moveBy(this.jumpDuration, cc.p(0, -this.jumpHeight)).easing(cc.easeCubicActionIn());
+        // 不断重复
+        return cc.repeatForever(cc.sequence(jumpUp, jumpDown));
+    }
+
+
+    start () {
+        cc.log(555);
+        this.node.runAction(this.setJumpAction())
+        
+    }
+
+    // update (dt) {},
+}
---
+++ library/imports/40/40545a86-1f27-44d5-9232-ae46e4cefa7c.js
@@ -0,0 +1,75 @@
+"use strict";
+cc._RF.push(module, '40545qGHydE1ZIyrkbkzvp8', 'player');
+// scripts/player.ts
+
+Object.defineProperty(exports, "__esModule", { value: true });
+var _a = cc._decorator, ccclass = _a.ccclass, property = _a.property;
+var NewClass = /** @class */ (function (_super) {
+    __extends(NewClass, _super);
+    function NewClass() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        // 主角跳跃高度
+        _this.jumpHeight = 0;
+        // 主角跳跃持续时间
+        _this.jumpDuration = 0;
+        // 辅助形变动作时间
+        _this.squashDuration = 0;
+        // 最大移动速度
+        _this.maxMoveSpeed = 0;
+        // 加速度
+        _this.accel = 0;
+        // 跳跃音效资源
+        _this.jumpAudio = '';
+        // 加速度方向开关
+        _this.accLeft = false;
+        _this.accRight = false;
+        // 主角当前水平方向速度
+        _this.xSpeed = 0;
+        // screen boundaries
+        _this.minPosX = 0;
+        _this.maxPosX = 0;
+        // 初始化跳跃动作
+        _this.jumpAction = null;
+        return _this;
+        // update (dt) {},
+    }
+    NewClass.prototype.setJumpAction = function () {
+        // 跳跃上升
+        var jumpUp = cc.moveBy(this.jumpDuration, cc.p(0, this.jumpHeight)).easing(cc.easeCubicActionOut());
+        // 下落
+        var jumpDown = cc.moveBy(this.jumpDuration, cc.p(0, -this.jumpHeight)).easing(cc.easeCubicActionIn());
+        // 不断重复
+        return cc.repeatForever(cc.sequence(jumpUp, jumpDown));
+    };
+    NewClass.prototype.start = function () {
+        cc.log(555);
+        this.node.runAction(this.setJumpAction());
+    };
+    __decorate([
+        property
+    ], NewClass.prototype, "jumpHeight", void 0);
+    __decorate([
+        property
+    ], NewClass.prototype, "jumpDuration", void 0);
+    __decorate([
+        property
+    ], NewClass.prototype, "squashDuration", void 0);
+    __decorate([
+        property
+    ], NewClass.prototype, "maxMoveSpeed", void 0);
+    __decorate([
+        property
+    ], NewClass.prototype, "accel", void 0);
+    __decorate([
+        property({
+            url: cc.AudioClip
+        })
+    ], NewClass.prototype, "jumpAudio", void 0);
+    NewClass = __decorate([
+        ccclass
+    ], NewClass);
+    return NewClass;
+}(cc.Component));
+exports.default = NewClass;
+
+cc._RF.pop();
